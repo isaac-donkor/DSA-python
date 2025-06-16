ac01496462e8fcc35d25bb03eac6e896fd57210b
Isaac Donkor <isaac.kwakye.donkor@outlook.com>
2025-06-16 10:23:10 +0000

    Created a list

---
+++ List.py
@@ -0,0 +1,15 @@
+# Creating a list
+fruits = ["apple", "banana", "cherry"]
+
+# Accessing elements
+print(fruits[0])   # apple
+
+# Adding elements
+fruits.append("orange")
+
+# Removing elements
+fruits.remove("banana")
+
+# Looping through list
+for fruit in fruits:
+    print(fruit)
