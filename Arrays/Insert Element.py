fb9f2e749c732297c8b857c6197b08aa1e133738
Isaac Donkor <isaac.kwakye.donkor@outlook.com>
2025-06-17 10:28:20 +0000

    update repo

---
+++ Arrays/Insert Element.py
@@ -0,0 +1,11 @@
+arr=[1,2,3,4,5]
+
+def insert(arr,index,successor):
+    for index in range(index,len(arr)):
+        arr[index],successor=successor,arr[index]
+
+        arr.append(successor)
+
+print("Array before insertion:",arr)
+insert(arr,3,4)
+print("Array after insertion:",arr)
